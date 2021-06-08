3e26a562518efecae021991c1a6cbd5698b1250b
Dennis Alberto Solano Padilla <dennis.solanopadilla@ucr.ac.cr>
2021-06-07 19:03:46 -0600

    Se modulariza, se crea una clase para el modal de selección de avatar. Se crea una clase que representa el waiting room. Se crea evento para que cuando se presione un avatar se le pase un id al modal en el data attribute...

--- design/source/js/waiting-room.js
+++ design/source/js/waiting-room.js
@@ -1,6 +1,13 @@
 const player_avatar_pos = 0;
 const player_name_pos = 1
 
+
+/*
+* On avatar clicked, set modal data attribute to player-avatar-n
+* On modal image clicked, get data attribute for player-avatar and change image
+* path with clicked avatar.
+*/
+
 class Player {
   constructor(avatarId, nameId) {
     this.avatarId = avatarId;
@@ -12,14 +19,14 @@ class Player {
 
 class PlayerList {
   constructor(id) {
-    this.id = id;
-    this.playersTable = document.getElementById(this.id);
+    this.playersTableId = id;
+    this.playersTable = document.getElementById(this.playersTableId);
     this.players = [];
   }
 
-  configurePlayersList() {
+  configurePlayersList(avatarSelector) {
     this.retrieveDataFromPlayersTable();
-    this.setupEventsForPlayersList();
+    this.setupEventsForPlayersList(avatarSelector);
   }
 
   retrieveDataFromPlayersTable() {
@@ -36,20 +43,51 @@ class PlayerList {
     this.players.push(player);
   }
 
-  setupEventsForPlayersList() {
+  setupEventsForPlayersList(avatarSelector) {
     for (let index = 0; index < this.players.length; index++) {
       this.players[index].avatarElement.addEventListener('click', () => {
-        const avatarSelection = document.getElementById('avatar-selection').style.display = 'block';
+        avatarSelector.show();
+        avatarSelector.setPlayersAvatarId(this.players[index].avatarId);
       });
     }
   }
 }
 
-function main() {
-  const avatarSelection = document.getElementById('avatar-selection').style.display = 'none';
+const avatar_selection_id = 'avatar-selection';
+class AvatarSelector {
+  constructor() {
+    this.element = document.getElementById(avatar_selection_id);
+    this.element.style.display = 'none';
+  }
 
-  const playerList = new PlayerList('players-list-table');
-  playerList.configurePlayersList();
+  show() {
+    this.element.style.display = 'block';
+  }
+
+  hide() {
+    this.element.style.display = 'none';
+  }
+
+  setPlayersAvatarId(id) {
+    this.element.dataset.avatarId = id;
+  }
+}
+
+const players_table_id = 'players-list-table';
+class WaitingRoom {
+  constructor() {
+    this.avatarSelector = new AvatarSelector();
+    this.playerList = new PlayerList(players_table_id);
+  }
+
+  configure() {
+    this.playerList.configurePlayersList(this.avatarSelector);
+  }
+}
+
+function main() {
+  waitingRoom = new WaitingRoom();
+  waitingRoom.configure();
 }
 
 window.addEventListener('load', main);
