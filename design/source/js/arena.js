61c4eb0279acd405ba89e4380a75f039a7af3181
Dennis Alberto Solano Padilla <dennis.solanopadilla@ucr.ac.cr>
2021-06-09 20:49:34 -0600

    Se implementa el cambio del color de carta que simula el haber jugado una carta y obtener una nueva del deck...

--- design/source/js/arena.js
+++ design/source/js/arena.js
@@ -1,118 +1,46 @@
-const player_start_row = 0;
-const player_start_col = 0;
+const PLAYERS_CARDS_TABLE_ID = 'players-cards';
+const CARDS_CELL = 2;
+const CARDS_COUNT = 3;
+const CARDS = [
+  'icons/cards/blue.svg',
+  'icons/cards/yellow.svg',
+  'icons/cards/red.svg',
+  'icons/cards/green.svg',
+  'icons/cards/orange.svg',
+  'icons/cards/purple.svg',
+  'icons/cards/meteorite.svg'];
 
-const game_board_rows = 8;
-const game_board_cols = 8;
+class PlayersCards {
+  constructor() {
+    this.tableBodyElement = document.getElementById(PLAYERS_CARDS_TABLE_ID);
+  }
 
-class Cell {
-    constructor(id, color) {
-        this.geyser = false;
-        this.egg = false;
-        this.binoculars = false;
-        this.color = color;
-    }
-}
+  configurePlayersCards() {
+    this.setupEventsForCards();
+  }
 
-class Board {
-    constructor(id) {
-        this.id = id;
-        let arr = new Array(game_board_cols);
-        for (let i = 0; i < arr.length; i++) {
-            arr[i] = new Array(game_board_rows);
-        }
-        for (let i = 0; i < game_board_cols; i++) {
-            for (let j = 0; j < game_board_rows; j++) {
-                arrp[i][j] = new Cell()
-            }
-        }
-    }
-
-}
-
-class Player {
-    constructor(avatarId, nameId) {
-        this.avatarId = avatarId;
-        this.nameId = nameId;
-        this.avatarElement = document.getElementById(avatarId);
-        this.nameElement = document.getElementById(nameId);
-        this.row = parseInt(this.element.dataset.row, 10);
-        this.col = parseInt(this.element.dataset.col, 10);
-    }
-
-    //busca el color
-    //mueve a ese color
-    move(color)
-}
-
-class Card {
-    constructor(id, color) {
-        this.id = id;
-        this.color = color;
-    }
-}
-class Player {
-    constructor(id) {
-        this.id = id;
-        this.element = document.getElementById(id);
-        console.assert(this.element);
-        // this.row = player_start_row;
-        // this.col = player_start_col;
-        this.row = parseInt(this.element.dataset.row, 10);
-        this.col = parseInt(this.element.dataset.col, 10);
-        this.move(0, 0);
-        this.element.addEventListener('click', (event) => {
-            console.assert(event);
-            this.move(0, -1);
-            this.element.classList.add('active_player');
-            window.setTimeout(() => {
-                this.element.classList.remove('active_player');
-            }, 4000);
+  setupEventsForCards() {
+    const playersCount = this.tableBodyElement.children.length;
+    for (let i = 0; i < playersCount; i += 1) {
+      const playersCards = this.tableBodyElement.children.item(i).children.item(CARDS_CELL);
+      for (let j = 0; j < CARDS_COUNT; j += 1) {
+        const card = playersCards.children[j];
+        card.addEventListener('click', () => {
+          // ToDo: Implement logic to move dinosaur.
+          card.src = this.getRandomCard();
         });
+      }
     }
+  }
 
-    move(delta_rows, delta_cols) {
-        const computedStyles = window.getComputedStyle(this.element);
-        console.log(`computedStyles.left = ${computedStyles.left}`);
-
-        console.log(`move player 1 from (${this.row}, ${this.col})`);
-        this.row += delta_rows;
-        this.col += delta_cols;
-        console.log(`move player 1 to (${this.row}, ${this.col})`);
-        const row_percent = 100.0 * this.row / game_board_rows;
-        const col_percent = 100.0 * this.col / game_board_cols;
-        console.log(`this.element.style.left = (${this.element.style.left})`);
-        this.element.style.top = `${row_percent}%`;
-        this.element.style.left = `${col_percent}%`;
-        console.log(`this.element.style.left = (${this.element.style.left})`);
-
-        console.log(`computedStyles.left = ${computedStyles.left}`);
-    }
-}
-
-class Game {
-    constructor() {}
-
-    setupEvents() {
-        this.player1 = new Player('player1');
-        this.setupHelp();
-    }
-
-    setupHelp() {
-        const button = document.getElementById('toggle_help');
-        button.addEventListener('click', () => {
-            const help = document.getElementById('help');
-            if (help.style.display === 'none') {
-                help.style.display = 'block';
-            } else {
-                help.style.display = 'none';
-            }
-        });
-    }
+  getRandomCard() {
+    return CARDS[Math.floor(Math.random() * CARDS.length)];
+  }
 }
 
 function main() {
-    const game = new Game();
-    game.setupEvents();
+  const playersCards = new PlayersCards();
+  playersCards.configurePlayersCards();
 }
 
 window.addEventListener('load', main);
