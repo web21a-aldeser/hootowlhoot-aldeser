41df511c83d2a5cd6ee79c8af5df6ab29b41878a
Dennis Alberto Solano Padilla <dennis.solanopadilla@ucr.ac.cr>
2021-07-10 10:21:17 -0600

    Se obtiene le host y puerto con window.location.host para inicializar el websocket

--- public/js/waiting-room/waiting-room-main.js
+++ public/js/waiting-room/waiting-room-main.js
@@ -1,8 +1,7 @@
 import WaitingRoom from './WaitingRoom.js';
 
 function main() {
-  const socket = new WebSocket('ws://localhost:3000');
-  localStorage.setItem('socket', socket);
+  const socket = new WebSocket(`ws://${window.location.host}`);
   const waitingRoom = new WaitingRoom(socket);
   waitingRoom.configure();
 }
